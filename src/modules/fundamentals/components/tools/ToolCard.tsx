6ded9c0aace78e2febec00225389906ea713ab28
Alejandro Lozano <alejandro20181999@gmail.com>
2026-01-28 22:35:53 -0300

    implementei carregamento de icones para os tools

--- src/modules/fundamentals/components/tools/ToolCard.tsx
+++ src/modules/fundamentals/components/tools/ToolCard.tsx
@@ -1,29 +1,41 @@
 import { Link } from "react-router-dom";
 import type { Tool } from "../../domain/tools/tool";
-import './ToolCard.css';
+import "./ToolCard.css";
+import { getToolIcon } from "../../ui/icons/toolIcons";
 
 type Props = { tool: Tool };
 
 export function ToolCard({ tool }: Props) {
-    return (
-        <article className="tool-card">
-            <div className="tool-card__icon" aria-hidden="true">
-                <span>🧰</span>
-            </div>
+  const iconSrc = getToolIcon(tool.id);
 
-            <div className="tool-card_content">
-                <div className="tool-card__titleRow">
-                    <h3 className="tool-card_title">
-                        <Link to={`/tools/${tool.id}`} className="tool-card__titleLink">
-                            {tool.name}
-                        </Link>
-                    </h3>
-                    
-                    <span className="tool-card__badge">{tool.category}</span>
-                </div>
+  return (
+    <article className="tool-card">
+      <div className="tool-card__icon" aria-hidden="true">
+        {
+            iconSrc
+                ? (<img
+                        className="tool-card__iconImg"
+                        src={iconSrc}
+                        width={28}
+                        height={28}
+                    />)
+                : (<span className="tool-card__iconText">🧰</span>)
+        }
+      </div>
 
-                <p className="tool-card__description">{tool.description}</p>
-            </div>
-        </article>
-    )
+      <div className="tool-card__content">
+        <div className="tool-card__titleRow">
+          <h3 className="tool-card__title">
+            <Link to={`/tools/${tool.id}`} className="tool-card__titleLink">
+              {tool.name}
+            </Link>
+          </h3>
+
+          <span className="tool-card__badge">{tool.category}</span>
+        </div>
+
+        <p className="tool-card__description">{tool.description}</p>
+      </div>
+    </article>
+  );
 }
